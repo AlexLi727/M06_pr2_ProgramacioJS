5432c7c6b5bb9675b947aebf48738d7eb7806bfb
AlexLi727 <alexavililee@gmail.com>
2024-10-03 03:28:58 +0200

    2

--- pr_02/Alabau.js
+++ pr_02/Alabau.js
@@ -1,27 +1,69 @@
 let Lista = document.getElementById("taula_propietats");
 Lista.innerHTML = `
-<table style = 100%>
+<table border = 1px solid black>
 <tr>
 <th> 1 </th> <th> 2 </th>
 </tr>
 
 <tr>
 <td>Valor máxim que pot tenir un número JS</td>
-<td>`+Number.MIN_VALUE+`</td>
+<td>`+Number.MAX_VALUE+`</td>
 </tr>
 
 <tr>
 <td>Altura total de la pantalla</td> 
-<td>`+screen.Height+`</td>
+<td>`+screen.height+`</td>
 </tr>
 
 <tr>
 <td> Altura interna de la finestra </td>
-<td>
+<td>`+window.innerHeight+`</td>
+</tr>
 
 <tr>
 <td>URL de la web</td>
 <td>`+document.URL+`</td>
+</tr>
 </table>
-
 `;
+
+
+
+function setTimer(){
+    let minutos = document.getElementById("minutos");
+    let segundos = document.getElementById("segundos");
+    let newMin = document.getElementById("setMin").value;
+    let newSec = document.getElementById("setSec").value.toString();
+    minutos.innerHTML = newMin
+    segundos.innerHTML = newSec
+    if(isNaN(parseInt(newMin))){
+        minutos.innerHTML = 0;
+    }
+    if(isNaN(parseInt(newSec))){
+        segundos.innerHTML = "00";
+    }
+}
+function startTimer(){
+        let minutos = parseInt(document.getElementById("minutos").textContent);
+        let segundos = parseInt(document.getElementById("segundos").textContent);
+    let countdown = window.setInterval(function (){
+        
+        segundos--;
+        if(segundos == 0 && minutos == 0){
+            clearInterval(countdown);
+        }
+        if(segundos == -1){
+            segundos = 59;
+            minutos--;
+            document.getElementById("minutos").innerHTML = minutos;
+        }
+        if(segundos == 0 && minutos == 0){
+            clearInterval(countdown);
+        }
+        document.getElementById("segundos").innerHTML = segundos;
+        
+
+        
+    }
+    ,1000)
+}
